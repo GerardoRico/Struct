aa3c4147df73ca895547cd5ca5707506f7518958
javier.rojas <javier.rojas@zonesoftware.co>
2016-04-09 11:47:17 -0500

    se elimina proyecto parasito

---
+++ src/java/com/tallerstruts/actions/DashboardAction.java
@@ -0,0 +1,12 @@
+package com.tallerstruts.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class DashboardAction extends ActionSupport {
+
+    @Override
+    public String execute() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
